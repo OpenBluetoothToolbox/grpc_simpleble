9b87290115698d684288c6e11d07ac1fa8b1d709
Kevin Dewald <5274600+kdewald@users.noreply.github.com>
2022-01-27 00:21:08 -0800

    Added code formatting and a non-linkable basic implementation.

--- server/main.cpp
+++ server/main.cpp
@@ -1,4 +1,29 @@
 
+#include <grpc/grpc.h>
+#include <grpcpp/security/server_credentials.h>
+#include <grpcpp/server.h>
+#include <grpcpp/server_builder.h>
+#include <grpcpp/server_context.h>
+#include "simplegrpcble.grpc.pb.h"
+
+class SimpleGRPCBleImpl final : public simplegrpcble::SimpleGRPCBLE::Service {
+  public:
+};
+
+void RunServer() {
+    std::string server_address("0.0.0.0:50051");
+    SimpleGRPCBleImpl service;
+
+    grpc::ServerBuilder builder;
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+    std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+    server->Wait();
+}
+
 int main(int argc, char** argv) {
+    RunServer();
+
     return 0;
 }
